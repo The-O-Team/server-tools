6c43fca0038dde89ff86f230e0d6d4349a0204aa
sebalix <seb@usr-src.org>
2015-10-30 17:24:16 +0100

    Module 'auditlog' - Log HTTP user sessions and requests

--- auditlog/models/http_request.py
+++ auditlog/models/http_request.py
@@ -1,38 +1,42 @@
-# Copyright 2015 ABF OSIELL <https://osiell.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 ABF OSIELL (<http://osiell.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
-from psycopg2.extensions import AsIs
-
-from odoo import api, fields, models
-from odoo.http import request
+from openerp import models, fields, api
+from openerp.http import request
 
 
 class AuditlogHTTPRequest(models.Model):
-    _name = "auditlog.http.request"
-    _description = "Auditlog - HTTP request log"
+    _name = 'auditlog.http.request'
+    _description = u"Auditlog - HTTP request log"
     _order = "create_date DESC"
 
-    display_name = fields.Char("Name", compute="_compute_display_name", store=True)
-    name = fields.Char("Path")
-    root_url = fields.Char("Root URL")
-    user_id = fields.Many2one("res.users", string="User")
+    name = fields.Char(u"Path")
+    root_url = fields.Char(u"Root URL")
+    user_id = fields.Many2one(
+        'res.users', string=u"User")
     http_session_id = fields.Many2one(
-        "auditlog.http.session", string="Session", index=True
-    )
-    user_context = fields.Char("Context")
-    log_ids = fields.One2many("auditlog.log", "http_request_id", string="Logs")
-
-    @api.depends("create_date", "name")
-    def _compute_display_name(self):
-        for httprequest in self:
-            create_date = fields.Datetime.from_string(httprequest.create_date)
-            tz_create_date = fields.Datetime.context_timestamp(httprequest, create_date)
-            httprequest.display_name = "{} ({})".format(
-                httprequest.name or "?", fields.Datetime.to_string(tz_create_date)
-            )
-
-    def name_get(self):
-        return [(request.id, request.display_name) for request in self]
+        'auditlog.http.session', string=u"Session")
+    user_context = fields.Char(u"Context")
+    log_ids = fields.One2many(
+        'auditlog.log', 'http_request_id', string=u"Logs")
 
     @api.model
     def current_http_request(self):
@@ -42,26 +46,17 @@ class AuditlogHTTPRequest(models.Model):
         first call.
         If no HTTP request is available, returns `False`.
         """
-        if not request:
-            return False
-        http_session_model = self.env["auditlog.http.session"]
+        http_session_model = self.env['auditlog.http.session']
         httprequest = request.httprequest
         if httprequest:
-            if hasattr(httprequest, "auditlog_http_request_id"):
-                # Verify existence. Could have been rolled back after a
-                # concurrency error
-                self.env.cr.execute(
-                    "SELECT id FROM %s WHERE id = %s",
-                    (AsIs(self._table), httprequest.auditlog_http_request_id),
-                )
-                if self.env.cr.fetchone():
-                    return httprequest.auditlog_http_request_id
+            if hasattr(httprequest, 'auditlog_http_request_id'):
+                return httprequest.auditlog_http_request_id
             vals = {
-                "name": httprequest.path,
-                "root_url": httprequest.url_root,
-                "user_id": request.uid,
-                "http_session_id": http_session_model.current_http_session(),
-                "user_context": request.context,
+                'name': httprequest.path,
+                'root_url': httprequest.url_root,
+                'user_id': request.uid,
+                'http_session_id': http_session_model.current_http_session(),
+                'user_context': request.context,
             }
             httprequest.auditlog_http_request_id = self.create(vals).id
             return httprequest.auditlog_http_request_id
--- auditlog/models/http_session.py
+++ auditlog/models/http_session.py
@@ -1,34 +1,38 @@
-# Copyright 2015 ABF OSIELL <https://osiell.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015 ABF OSIELL (<http://osiell.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
-from odoo import api, fields, models
-from odoo.http import request
+from openerp import models, fields, api
+from openerp.http import request
 
 
 class AuditlogtHTTPSession(models.Model):
-    _name = "auditlog.http.session"
-    _description = "Auditlog - HTTP User session log"
+    _name = 'auditlog.http.session'
+    _description = u"Auditlog - HTTP User session log"
     _order = "create_date DESC"
 
-    display_name = fields.Char("Name", compute="_compute_display_name", store=True)
-    name = fields.Char("Session ID", index=True)
-    user_id = fields.Many2one("res.users", string="User", index=True)
+    name = fields.Char(u"Session ID")
+    user_id = fields.Many2one(
+        'res.users', string=u"User")
     http_request_ids = fields.One2many(
-        "auditlog.http.request", "http_session_id", string="HTTP Requests"
-    )
-
-    @api.depends("create_date", "user_id")
-    def _compute_display_name(self):
-        for httpsession in self:
-            create_date = fields.Datetime.from_string(httpsession.create_date)
-            tz_create_date = fields.Datetime.context_timestamp(httpsession, create_date)
-            httpsession.display_name = "{} ({})".format(
-                httpsession.user_id and httpsession.user_id.name or "?",
-                fields.Datetime.to_string(tz_create_date),
-            )
-
-    def name_get(self):
-        return [(session.id, session.display_name) for session in self]
+        'auditlog.http.request', 'http_session_id', string=u"HTTP Requests")
 
     @api.model
     def current_http_session(self):
@@ -38,16 +42,17 @@ class AuditlogtHTTPSession(models.Model):
         first call.
         If no HTTP user session is available, returns `False`.
         """
-        if not request:
-            return False
-        httpsession = request.session
+        httpsession = request.httpsession
         if httpsession:
             existing_session = self.search(
-                [("name", "=", httpsession.sid), ("user_id", "=", request.uid)], limit=1
-            )
+                [('name', '=', httpsession.sid),
+                 ('user_id', '=', request.uid)])
             if existing_session:
                 return existing_session.id
-            vals = {"name": httpsession.sid, "user_id": request.uid}
+            vals = {
+                'name': httpsession.sid,
+                'user_id': request.uid,
+            }
             httpsession.auditlog_http_session_id = self.create(vals).id
             return httpsession.auditlog_http_session_id
         return False
