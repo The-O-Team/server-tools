c9c995497c456703407cfd4516b484604efc6149
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-02-21 11:00:55 +0100

    [ADD] split module sale_exception into base_exception

--- base_exception/models/__init__.py
+++ base_exception/models/__init__.py
@@ -1,3 +1,5 @@
-from . import exception_rule
-from . import base_exception_method
+# -*- coding: utf-8 -*-
+# © 2011 Raphaël Valyi, Renato Lima, Guewen Baconnier, Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import base_exception
