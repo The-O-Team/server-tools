5fa7c454892ec2ca04daf1d7685523626301bdf3
Alexis de Lattre <alexis.delattre@akretion.com>
2017-02-27 14:11:58 +0100

    [MIG] module onchange_helper to v10.0

--- onchange_helper/__manifest__.py
+++ onchange_helper/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2016-2017 Akretion (http://www.akretion.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# -*- coding: utf-8 -*-
+# © 2016-2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-{
-    "name": "Onchange Helper",
-    "version": "17.0.1.0.1",
-    "summary": "Technical module that ease execution" " of onchange in Python code",
-    "author": "Akretion,Camptocamp,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/server-tools",
-    "license": "AGPL-3",
-    "category": "Generic Modules",
-    "depends": ["web"],
-    "installable": True,
-}
+{'name': 'Onchange Helper',
+ 'version': '10.0.1.0.0',
+ 'summary': 'Technical module that ease execution of onchange in Python code',
+ 'author': 'Akretion,Camptocamp,Odoo Community Association (OCA)',
+ 'website': 'http://www.akretion.com',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': ['base'],
+ 'installable': True,
+ }
