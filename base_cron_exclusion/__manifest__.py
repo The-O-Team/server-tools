4d029f2d8609a0962a83bad66713c6eb365298eb
Lois Rilo <lois.rilo@eficent.com>
2018-02-08 16:40:49 +0100

    [10.0][MIG] base_cron_exclusion (#1105)
    * [9.0][ADD] base_cron_exclusion
    
    * [10.0][MIG] base_cron_exclusion
    
    [11.0][MIG] base_cron_exclusion

--- base_cron_exclusion/__manifest__.py
+++ base_cron_exclusion/__manifest__.py
@@ -1,18 +1,21 @@
-# Copyright 2017-21 ForgeFlow S.L. (https://www.forgeflow.com)
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Base Cron Exclusion",
     "summary": "Allow you to select scheduled actions that should not run "
-    "simultaneously.",
-    "version": "17.0.1.0.0",
-    "author": "ForgeFlow, Odoo Community Association (OCA)",
-    "maintainers": ["LoisRForgeFlow", "ChrisOForgeFlow"],
-    "development_status": "Production/Stable",
+               "simultaneously.",
+    "version": "11.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-tools",
     "category": "Tools",
-    "depends": ["base"],
-    "data": ["views/ir_cron_view.xml"],
-    "license": "LGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/ir_cron_view.xml",
+    ],
+    "license": "AGPL-3",
     "installable": True,
 }
