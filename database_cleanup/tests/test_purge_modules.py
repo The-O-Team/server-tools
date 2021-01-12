6332a38aadd3182ed2b19c861e18703dee6f01f7
Stephane Mangin <stephane.mangin@camptocamp.com>
2021-01-12 14:28:34 +0100

    [MIG] database_cleanup: Migration to 14.0

--- database_cleanup/tests/test_purge_modules.py
+++ database_cleanup/tests/test_purge_modules.py
@@ -1,29 +1,35 @@
 # Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import threading
 
-from odoo.tests.common import tagged
+from odoo.tools import config
 
-from .common import Common, environment
+from .common import Common
 
 
-# Use post_install to get all models loaded more info: odoo/odoo#13458
-@tagged("post_install", "-at_install")
 class TestCleanupPurgeLineModule(Common):
     def setUp(self):
-        super().setUp()
-        with environment() as env:
-            # create a nonexistent module
-            self.module = env["ir.module.module"].create(
-                {
-                    "name": "database_cleanup_test",
-                    "state": "to upgrade",
-                }
-            )
+        super(TestCleanupPurgeLineModule, self).setUp()
+        # create a nonexistent module
+        self.module = self.env["ir.module.module"].create(
+            {
+                "name": "database_cleanup_test",
+                "state": "to upgrade",
+            }
+        )
 
     def test_remove_to_upgrade_module(self):
-        with environment() as env:
-            wizard = env["cleanup.purge.wizard.module"].create({})
-            module_names = wizard.purge_line_ids.filtered(
-                lambda x: not x.purged
-            ).mapped("name")
-            self.assertTrue("database_cleanup_test" in module_names)
+        wizard = self.env["cleanup.purge.wizard.module"].create({})
+        config.options["test_enable"] = False  # Maybe useless now ?!
+        self.patch(threading.currentThread(), "testing", False)
+        wizard.purge_all()
+        config.options["test_enable"] = True  # Maybe useless now ?!
+        self.patch(threading.currentThread(), "testing", True)
+        # must be removed by the wizard
+        self.assertFalse(
+            self.env["ir.module.module"].search(
+                [
+                    ("name", "=", "database_cleanup_test"),
+                ]
+            )
+        )
